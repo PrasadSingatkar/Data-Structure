85388cb7da3222cb25756aa0729bb72f232edf67
The Prasad Singatkar <prasadsingatkar360@gmail.com>
2023-12-08 00:58:51 +0530

    AdvanceJavaFirst

---
+++ ADVJAVA/Last/day1_lab/src/main/java/pages/SecondServlet.java
@@ -0,0 +1,59 @@
+package pages;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+
+public class SecondServlet extends HttpServlet
+/*
+ * WC creates a map Key : /test2 Value : pages.SecondServlet
+ */
+{
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init()
+	 */
+	public void init() throws ServletException {
+		System.out.println("in init " + Thread.currentThread().getName());
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		System.out.println("in destroy " + Thread.currentThread().getName());
+
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("in do-get " + Thread.currentThread().getName());
+		// 1. set resp content type
+		response.setContentType("text/html");
+		// 2. Get PW to send buffered char resp from servlet --> clnt
+		try (PrintWriter pw = response.getWriter()) {
+			// 3. generate dyn resp
+			pw.print("<h5> Welcome to Servlet " + getClass() 
+			+ " : " + LocalDateTime.now() + " </h5>");
+
+		}
+
+	}
+
+}
